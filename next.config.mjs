056ee66e4e0da1e3f5ca9199d5c0cc619fc242a7
Swasthika-P-S <swasthikaponnusamy@gmail.com>
2026-02-21 09:35:50 +0530

    modified docker-compose.yml

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "standalone",
-    eslint: {
-        // Warning: This allows production builds to successfully complete even if
-        // your project has ESLint errors.
-        ignoreDuringBuilds: true,
-    },
-    typescript: {
-        // !! WARN !!
-        // Dangerously allow production builds to successfully complete even if
-        // your project has type errors.
-        ignoreBuildErrors: true,
-    },
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors or type errors.
+    eslint: { ignoreDuringBuilds: true },
+    typescript: { ignoreBuildErrors: true },
 };
 
 export default nextConfig;
